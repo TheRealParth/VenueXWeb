8a550f5b20e70b0e5684dbd27a63933a6f4f823f
psyrotix <psyrotix@gmail.com>
2018-12-23 14:21:10 -0500

    ordering staff functional

--- src/components/StaffTable/StaffTable.jsx
+++ src/components/StaffTable/StaffTable.jsx
@@ -1,19 +1,124 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Table from '@material-ui/core/Table';
+// import Table from '@material-ui/core/Table';
 import Grid from '@material-ui/core/Grid';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import Checkbox from '../Checkbox.js';
+import AddButton from '../AddButton.js';
+import Table from '../Table';
 import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
 import moment from 'moment';
 import UserAvatar from './UserAvatar';
 import PermissionsIcons from '../PermissionsIcons';
 import ManageStaffHeader from './ManageStaffHeader.jsx';
 import CaretDown from '../../assets/icons/CaretDown';
-import { rowHeaderTitle, hide, reverse, caretContainer } from './styles.module.scss';
+import Button from '../Button';
+import EditStaffPermissionsDropdown from '../staff/EditStaffPermissionsDropdown';
+
+import { rowHeaderTitle, rowValue, hide, reverse, caretContainer } from './styles.module.scss';
 import classnames from 'classnames';
+import Icons from '../../assets/icons';
+import ConsultantLabel from '../Consultant';
+
+const Container = styled.div`
+  border: solid 1px #ededed;
+  background-color: #fafafa;
+  padding: 0px 15px;
+`;
+const IconsContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+
+  svg {
+    margin: 0px 5px;
+  }
+`;
+
+const StyledTableRow = styled(Table.Row)`
+  .actions {
+    display: none;
+
+    svg {
+      margin: 0px 3px;
+    }
+  }
+
+  &:hover {
+    .actions {
+      display: block;
+    }
+  }
+`;
+
+const MonthPicker = styled.div`
+  display: flex;
+  font-size: 20px;
+  color: #222222;
+  align-items: center;
+`;
+
+const ArrowIcon = styled.img`
+  height: 17px;
+  object-fit: contain;
+  margin: 0px 20px;
+  cursor: pointer;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 60px;
+`;
+
+const Stats = styled.div`
+  background-color: #fff;
+  margin: 15px 0px;
+  border: solid 1px #fafafa;
+  border-radius: 2px;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Stat = styled.div`
+  color: #181818;
+  font-size: 15px;
+  padding-left: 30px;
+  margin-left: 30px;
+  border-left: solid 1px #b0b0b0;
+  font-family: Lora;
+  font-weight: 500;
+
+  &:first-child {
+    border-left: none;
+    margin-left: 0px;
+    padding-left: 0px;
+  }
+
+  .label {
+    color: #b0b0b0;
+    font-weight: 500;
+  }
+
+  .value {
+    font-size: 40px;
+  }
+`;
+
+const TableContainer = styled.div`
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
+  color: #222222;
+  margin-bottom: 20px;
+`;
+
+const DueLabel = styled.span`
+  color: ${props => props.color || '#b0b0b0'};
+`;
 
 const StaffTable = ({ users, classes: { paper, root, table, tableCell, ...classes }, ...rest }) => {
   console.log(rest);
@@ -25,115 +130,135 @@ const StaffTable = ({ users, classes: { paper, root, table, tableCell, ...classe
           <ManageStaffHeader {...rest} classes={classes} className={paper} />
         </Grid>
       </Grid>
-      <Paper className={root}>
+      <TableContainer>
         <Table className={table}>
-          <TableHead>
-            <TableRow>
-              <TableCell onClick={() => setUsersSortKey('fullName', sort.orderBy === 'asc' ? 'desc' : 'asc')} className={tableCell}>
-                <p
-                  className={classnames({
-                    [rowHeaderTitle]: true
-                  })}
-                >
-                  Name
-                </p>
-                <div
-                  className={classnames({
-                    [caretContainer]: true,
-                    [reverse]: sort.sortKey === 'fullName' && sort.orderBy === 'asc',
-                    [hide]: !(sort.sortKey === 'fullName')
-                  })}
-                >
-                  <CaretDown size={14} />
-                </div>
-              </TableCell>
-              <TableCell onClick={() => setUsersSortKey('email', sort.orderBy === 'asc' ? 'desc' : 'asc')} className={tableCell} numeric>
-                <p
-                  className={classnames({
-                    [rowHeaderTitle]: true
-                  })}
-                >
-                  Email
-                </p>
-                <div
-                  className={classnames({
-                    [caretContainer]: true,
-                    [reverse]: sort.sortKey === 'email' && sort.orderBy === 'asc',
-                    [hide]: !(sort.sortKey === 'email')
-                  })}
-                >
-                  <CaretDown size={14} />
-                </div>
-              </TableCell>
-              <TableCell onClick={() => { }} className={tableCell} numeric>
-                <p
-                  className={classnames({
-                    [rowHeaderTitle]: true
-                  })}
-                >
-                  Permissions
-                </p>
-                <div
-                  className={classnames({
-                    [caretContainer]: true,
-                    [reverse]: sort.sortKey === 'permissions' && sort.orderBy === 'asc',
-                    [hide]: !(sort.sortKey === 'permissions')
-                  })}
-                >
-                  <CaretDown size={14} />
-                </div>
-              </TableCell>
-              <TableCell onClick={() => setUsersSortKey('created', sort.orderBy === 'asc' ? 'desc' : 'asc')} className={tableCell} numeric>
-                <p
-                  className={classnames({
-                    [rowHeaderTitle]: true
-                  })}
-                >
-                  Date Added
-                </p>
-                <div
-                  className={classnames({
-                    [caretContainer]: true,
-                    [reverse]: sort.sortKey === 'created' && sort.orderBy === 'asc',
-                    [hide]: !(sort.sortKey === 'created')
-                  })}
-                >
-                  <CaretDown size={14} />
-                </div>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map(user => (
-              <TableRow key={user.id}>
-                <TableCell className={tableCell} component="th" scope="row">
-                  <UserAvatar classes={rest} user={user} />
-                </TableCell>
-                <TableCell className={tableCell} numeric>
-                  {user.email}
-                </TableCell>
-                <TableCell className={tableCell} numeric>
-                  <PermissionsIcons {...user.permissions} />
-                </TableCell>
-                <TableCell className={tableCell} numeric>
-                  {`${moment(user.created).format('MM/DD/YYYY')}`}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
+          <Table.Row>
+            <Table.Cell width="5%">
+              <Table.HeaderCell
+                title={<Checkbox onCheck={() => { }} onUncheck={() => { }} checked={true} />}
+              />
+            </Table.Cell>
+            {false ?
+              <>
+                <Table.Cell width="20%">
+                  <Table.HeaderCell
+                    onClick={() => setUsersSortKey('fullName', sort.orderBy === 'asc' ? 'desc' : 'asc')}
+                    className={tableCell}
+                    title={'Name'}
+                  />
+                </Table.Cell>
+                <Table.Cell width="20%">
+                  <Table.HeaderCell
+                    onClick={() => setUsersSortKey('email', sort.orderBy === 'asc' ? 'desc' : 'asc')}
+                    className={tableCell}
+                    numeric
+                    title="Email"
+                  />
+                </Table.Cell>
+                <Table.Cell width="20%">
+                  <Table.HeaderCell onClick={() => { }} className={tableCell} title="Permissions" />
+                </Table.Cell>
+                <Table.Cell width="20%">
+                  <Table.HeaderCell
+                    onClick={() => setUsersSortKey('created', sort.orderBy === 'asc' ? 'desc' : 'asc')}
+                    className={tableCell}
+                    title="Date Added"
+                  />
+                </Table.Cell>
+   
+              </> 
+              :
+            <>
+              <Table.Cell width="80%">
+                <EditStaffPermissionsDropdown
+                  selectedEmployees={this.getSelectedEmployees()}
+                />
+
+                &nbsp;
+  
+            <Button
+                  label="Delete 2 staff members"
+                  kind="danger"
+                />
+              </Table.Cell>
+            </>
+            }
+                       </Table.Row>
+          <Table.Body>
+              {users.map(user => (
+                <StyledTableRow key={user.id}>
+                  <Table.Cell width="5%">
+                    <Checkbox onCheck={() => { }} onUncheck={() => { }} checked={false} />
+                  </Table.Cell>
+                  <Table.Cell width="20%">
+                    <ConsultantLabel name={user.fullName} picture={user.picture} />
+                  </Table.Cell>
+
+                  <Table.Cell width="20%">{user.email}</Table.Cell>
+
+                  <Table.Cell width="20%">
+                    <IconsContainer>
+                      <Icons.CalendarEdit
+                        size={24}
+                        color={!user.permissions.createAndEditEvents ? '#d8d8d8' : undefined}
+                      />
+                      <Icons.CalendarDelete
+                        size={24}
+                        color={!user.permissions.deleteEvents ? '#d8d8d8' : undefined}
+                      />
+                      <Icons.Billing
+                        size={24}
+                        color={!user.permissions.viewBilling ? '#d8d8d8' : undefined}
+                      />
+                      <Icons.ManageStaff
+                        size={24}
+                        color={!user.permissions.manageStaffPermissions ? '#d8d8d8' : undefined}
+                      />
+                    </IconsContainer>
+                  </Table.Cell>
+
+                  <Table.Cell width="20%">{moment(user.createdAt).format('MM/DD/YYYY')}</Table.Cell>
+
+                  <Table.Cell width="15%">
+                    <div className="actions">
+                      <Icons.Delete size={24} color="#7d7d7d" />
+                      <Icons.Delete size={24} color="#7d7d7d" />
+                    </div>
+                  </Table.Cell>
+                </StyledTableRow>
+                // <Table.Row key={user.id}>
+                //   <Table.Cell
+                //     className={classnames([tableCell, rowValue])}
+                //     component="th"
+                //     scope="row"
+                //   >
+                //     <UserAvatar classes={rest} user={user} />
+                //   </Table.Cell>
+                //   <Table.Cell className={classnames([tableCell, rowValue])} numeric>
+                //     {user.email}
+                //   </Table.Cell>
+                //   <Table.Cell className={classnames([tableCell, rowValue])} numeric>
+                //     <PermissionsIcons {...user.permissions} />
+                //   </Table.Cell>
+                //   <Table.Cell className={classnames([tableCell, rowValue])} numeric>
+                //     {`${moment(user.created).format('MM/DD/YYYY')}`}
+                //   </Table.Cell>
+                // </Table.Row>
+              ))}
+            </Table.Body>
         </Table>
-      </Paper>
+      </TableContainer>
     </>
-  );
-};
-
+      );
+    };
+    
 StaffTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  users: PropTypes.array.isRequired
-};
-
+        classes: PropTypes.object.isRequired,
+      users: PropTypes.array.isRequired
+    };
+    
 StaffTable.defaultProps = {
-  users: []
-};
-
-export { StaffTable };
+        users: []
+    };
+    
+export {StaffTable};
