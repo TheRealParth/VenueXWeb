befcd5b488617d1f9e74c842043f6cfe62d7bfad
Hassan Mahmoud <hass@Hassans-MacBook-Air.local>
2019-01-15 20:17:59 -0500

    fix contact color

--- src/components/InjectStyles/InjectStyles.jsx
+++ src/components/InjectStyles/InjectStyles.jsx
@@ -3,15 +3,13 @@ import { ContactInfo } from '../BillingHeader/index.module.scss';
 const InjectStyles = ({ colors }) => (
   <style>{`
     .${ContactInfo} {
-      color: ${colors.primary};
+     
     }
   `}</style>
 );
 
 InjectStyles.defaultProps = {
-  colors: {
-
-  }
-}
+  colors: {}
+};
 
 export { InjectStyles };
